c55a15d11780f0875e564c58c493efe438a98084
gintoki05 <ajieprastyo@gmail.com>
2023-05-31 09:37:57 +0700

    buat hook usefetch dan implementasi di info

--- components/info/Info.jsx
+++ components/info/Info.jsx
@@ -1,39 +1,43 @@
-import { Pressable, ScrollView, View } from 'react-native';
+import { FlatList, Pressable, ScrollView, View } from 'react-native';
 import { Image } from 'expo-image';
 
 import styles from './info.style';
 import { useRouter } from 'expo-router';
+import useFetch from '../../hook/useFetch';
+import { BASE_URL } from '../../utils/http';
 
 const Info = () => {
   const router = useRouter();
 
-  return (
-    <ScrollView>
-      <Pressable
-        onPress={() => {
-          router.push('/info/1');
-        }}
-      >
-        <View style={styles.container}>
-          <View style={styles.cardContainer}>
-            <Image
-              style={styles.image}
-              source='https://picsum.photos/seed/696/3000/2000'
-              contentFit='cover'
-              transition={1000}
-            />
-          </View>
-          <View style={styles.cardContainer}>
-            <Image
-              style={styles.image}
-              source='https://picsum.photos/seed/696/3000/2000'
-              contentFit='cover'
-              transition={1000}
-            />
-          </View>
+  const { data, isLoading, error } = useFetch('api/banners', { populate: '*' });
+
+  const Item = ({ image }) => (
+    <Pressable
+      onPress={() => {
+        router.push('/info/1');
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <Image
+            style={styles.image}
+            source={BASE_URL + image}
+            contentFit='cover'
+            transition={1000}
+          />
         </View>
-      </Pressable>
-    </ScrollView>
+      </View>
+    </Pressable>
+  );
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={({ item }) => (
+        <Item image={item.attributes.gambar.data.attributes.url} />
+      )}
+      keyExtractor={(item) => item.id}
+    />
   );
 };
 
---
+++ hook/useFetch.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: 'GET',
+    url: `http://localhost:1337/${endpoint}`,
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('There is an error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
